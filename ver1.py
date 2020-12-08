d181c419d4ef2d74a1a5858f46196e662de53d58
Anh Tuan Chau <73784610+tuananhc@users.noreply.github.com>
2020-12-08 23:29:48 +0700

    Update ver1.py

--- ver1.py
+++ ver1.py
@@ -1,15 +1,16 @@
+from IPython.display import clear_output
+
 def player_position(): 
     position = input('Your move: ')
-    while int(position) not in range(1,10) or not position.isdigit():
+    while not position.isdigit() or int(position)  not in range(1, 9):
         print('Invalid move!')
         position = input('Your move: ')
-    position = int(position)
-    return position
+    return int(position)
 
 def full_board_check(board): 
     if ' ' not in board: 
         print("No more moves available!")
-        return False
+        return True
 
 def replay():
     answer = input('Do you want to play again?\nY or N? ')
@@ -18,33 +19,37 @@ def replay():
         answer = input('Do you want to play again?\nY or N? ')
     if answer.upper() == 'Y':
         return True
-    else:
-        return False
+    return False
 
 def space_check(board, position):
-    while board[position] == ' ':
+    if board[position] != ' ':
         print("Can't place here!")
-        player_position()
+        return False
+    return True
 
 def player_marker():
     marker_1 = input('Do you want to be X or O? ').upper()
-    while marker_1 not in ('X','O'):
+    while marker_1 not in 'XO':
         print('Choose a valid marker')
         marker_1 = input('Do you want to be X or O? ').upper()
     marker_2 = 'X' if marker_1 == 'O' else 'O'
     return marker_1, marker_2
 
 def win_check(board,mark):
-    if board[0:3]==[mark, mark, mark] or board[3:6]==[mark, mark, mark] or board [6:9] ==[mark,mark,mark]:
-        return False
-    elif [board[0],board[3],board[6]]==[mark, mark, mark] or [board[1],board[4],board[7]]==[mark, mark, mark] or [board[2],board[5],board[8]]==[mark, mark, mark]:
-        return False
-    elif [board[0],board[4],board[8]]==[mark, mark, mark] or [board[2],board[4],board[6]]==[mark, mark, mark]:
-        return False
+    if (board[0:3] == [mark, mark, mark] or board[3:6] == 
+       [mark, mark, mark] or board [6:9] == [mark,mark,mark]):
+        return True
+    elif ([board[0],board[3],board[6]] == [mark, mark, mark] or 
+         [board[1],board[4],board[7]] == [mark, mark, mark] or 
+         [board[2],board[5],board[8]] == [mark, mark, mark]):
+        return True
+    elif ([board[0],board[4],board[8]] == [mark, mark, mark] or 
+         [board[2],board[4],board[6]] == [mark, mark, mark]) :
+        return True
+    return False
 
 def place_marker(board, marker, position):
-    while space_check(board, position):
-        board[position - 1] = marker
+    board[position - 1] = marker
 
 def display_board(board):
     print(board[6] + ' ' + '|' + ' ' + board[7] + ' ' + '|' + ' ' + board[8])
@@ -54,49 +59,44 @@ def display_board(board):
 def tic_tac_toe():
     print('Welcone to tic tac toe')
     while True:
+        marker_1, marker_2 = player_marker()
         board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-        player_marker()
+        print('Positions in board: ')
         demoboard = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         display_board(demoboard)
         print('-' * 9)
         turn = 1
-        marker_1, marker_2 = player_marker()
         while turn > 0: 
             if turn % 2 != 0: 
                 #Player 1 turn
-                if not full_board_check(board):
+                if full_board_check(board):
                     break
                 position = player_position()
-                while not space_check(board,position):
+                while not space_check(board, position):
                     player_position()
-                while not space_check(board,position):
-                    player_position()
-                place_marker(board,marker_1,position)
+                place_marker(board, marker_1, position)
                 display_board(board)
                 print('-' * 9)
-                if not win_check(board, marker_1):
-                    break
-                if not win_check(board, marker_2):
+                if win_check(board, marker_1):
+                    print("Player 1 wins!")
                     break
                 turn += 1
             else: 
                 #Player 2 turn
-                if not full_board_check(board):
+                if full_board_check(board):
                     break
                 position = player_position()
-                while not space_check(board,position):
+                while not space_check(board, position):
                     player_position()
-                while not space_check(board,position):
-                    player_position()
-                place_marker(board,marker_2,position)
+                place_marker(board, marker_2, position)
                 display_board(board)
                 print('-' * 9)
-                if not win_check(board, marker_1):
-                    break
-                if not win_check(board, marker_2):
+                if win_check(board, marker_2):
+                    print("Player 2 wins!")
                     break
                 turn += 1
-    print('Bye')
-    return
-
-tic_tac_toe()
+        if replay(): 
+            clear_output()
+        else: 
+            print('Bye')
+            return
